5b100e24c1094efdd706dc54ca0901bbdf9d9996
Gustavo Cunha Bueno Giavera <x523970@rede.sp>
2026-01-16 10:13:17 -0300

    Segunda Conexão CSS

---
+++ gestao-documental.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/header-pc.css">
+    <link rel="stylesheet" href="/css/footer-pc.css">
+    <link rel="stylesheet" href="/css/main-pc.css">
+    <title>Política de Gestão Documental</title>
+</head>
+
+<body>
+
+    <?php require_once __DIR__ . "/views/header-pc.php"
+        ?>
+
+    <div class="banner-estrutura">
+        <img src="/assets/gestao-documental/banner-gest-doc.png" alt="Banner">
+    </div>
+
+
+    <div class="container-pagina">
+
+        <div class="card-secao">
+            <p>
+                Aqui estão alguns dos principais pontos abordados pela portaria:
+            </p>
+
+            <h4> Objetivos e Princípios</h4>
+            <p>A portaria estabelece a política de gestão documental com base em princípios como a autenticidade,
+                acessibilidade, integridade, e segurança dos documentos. O objetivo é assegurar que os documentos sejam
+                gerenciados de maneira eficaz ao longo de seu ciclo de vida, desde a sua produção ou recebimento até sua
+                eliminação, quando necessário.</p>
+
+            <h4>Responsabilidades</h4>
+            <p>A portaria define responsabilidades claras para os órgãos e entidades municipais, incluindo a
+                implementação de programas de gestão de documentos, treinamentos para servidores públicos, e a
+                designação de profissionais responsáveis pela gestão documental. Isso inclui a criação das Equipes de
+                Gestão Documentação- Edoc, como parceiras e representantes das Secretarias nas aplicações de normas,
+                diretrizes e boas práticas em relação a gestão de documentos e arquivos correntes.</p>
+
+            <h4>Planejamento e Acompanhamento</h4>
+            <p>A implementação da gestão documental deve ser acompanhada por planos específicos para cada órgão. Isso
+                inclui a elaboração de planos de ação para o manejo dos documentos, assegurando que estejam em
+                conformidade com as políticas de preservação e eliminação. O acompanhamento deve ser contínuo, com
+                relatórios e auditorias periódicas.</p>
+
+            <h4>Ciclo de Vida dos Documentos</h4>
+            <p>A portaria estabelece um procedimento para a gestão do ciclo de vida dos documentos. Isso implica:</p>
+
+            <div class="lista-txt">
+                <ul>
+                    <li>Classificação e organização: Identificar e classificar os documentos de acordo com sua natureza
+                        e importância.</li>
+                    <li>Tratamento: Garantir que os documentos sejam acessíveis, conservados adequadamente e que tenham
+                        sua integridade preservada.</li>
+                    <li>Tabela de Temporalidade e Eliminação: Definir os critérios e formalização da TTD, com definições
+                        de prazos e destinação, sendo a guarda permanente ou eliminação, devendo seguir aos normativos
+                        específicos para descarte de documentos.</li>
+                </ul>
+            </div>
+
+            <h4>Capacitação</h4>
+            <p>A capacitação de servidores e gestores para a gestão documental é fundamental, e a portaria prevê a
+                realização de treinamentos periódicos para assegurar que as boas práticas sejam seguidas em todos os
+                níveis da administração pública.</p>
+
+            <h4>Avaliação e Revisão</h4>
+            <p>A eficácia da política de gestão documental deve ser revisada periodicamente, com avaliações constantes
+                sobre sua implementação e eficácia, permitindo ajustes conforme as necessidades e desafios que surgem
+            </p>
+
+
+
+        </div>
+
+
+
+    </div>
+
+    <?php require_once __DIR__ . "/views/footer-pc.php"
+    ?>
+
+
+
+</body>
+
+</html>
---
+++ views/footer.php
@@ -0,0 +1,17 @@
+ <footer class="footer">
+     <div class="conteiner">
+         <div class="footer-texto">
+             <p class="p-10">Secretaria Municipal de Gestão</p>
+             <p class="p-10"><strong>ARQUIVO PÚBLICO MUNICIPAL</strong><br>Jornalista Paulo Roberto Dutra</p>
+             <a href="/">
+                 <img class="p-10" src="/assets/footer/icone-arquip.png" alt="Arquip" />
+             </a>
+         </div>
+         <div class="footer-logo">
+             <a href="https://www.prefeitura.sp.gov.br/cidade/secretarias/gestao/">
+                 <img src="/assets/footer/icone-gestao.png" alt="Cidade de São Paulo" />
+             </a>
+         </div>
+     </div>
+     <p class="p-10">R. Léo Ribeiro de Morães, 66 – Freguesia do Ó<br>São Paulo/SP – Brasil</p>
+ </footer>
