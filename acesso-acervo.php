df4660fd41b9c14d64ded23ada12f2794177f291
GuGiavera <gustavobuenosk@gmail.com>
2026-01-28 12:50:28 -0300

    Termino da pagina Acesso_Acervo e Diario-Oficial

---
+++ acesso-acervo.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/CSS/header.css">
+    <link rel="stylesheet" href="/CSS/footer.css">
+    <link rel="stylesheet" href="/CSS/main-pc.css">
+    <link rel="stylesheet" href="/css/acesso-acervo.css">
+    <link rel="shortcut icon" href="/assets/icone.site/Logo.ico" type="image/x-icon">
+    <title>Acesso Ao Acervo</title>
+</head>
+
+<body>
+
+    <?php require_once __DIR__ . "/views/header.php"
+        ?>
+
+    <div class="banner-estrutura">
+
+        <div>
+            <img class="img-esquerda" src="/assets/acesso-acervo/img-acervo.png" alt="Acervo">
+        </div>
+
+        <div class="texto-banner">
+            <h1>ACESSO AO ACERVO - REQUERIMENTO ELETRÔNICO</h1>
+            <p>O ARQUIP disponibiliza um serviço digital para que cidadãos e empresas possam solicitar acesso ao acervo
+                documental de processos do Arquivo Público Municipal.
+            </p>
+        </div>
+
+        <div>
+            <img class="img-direita" src="/assets/acesso-acervo/icone-acervo.png" alt="Icone">
+        </div>
+    </div>
+
+    <div class="container-pagina">
+        <div class="card-secao">
+            <div class="img-acervo">
+                <img class="img-frente" src="/assets/acesso-acervo/img-frente.png" alt="Imagem Frente">
+                <img class="img-fundo" src="/assets/acesso-acervo/img-fundo.png" alt="Imagem Fundo">
+                <img class="caixa-fundo" src="/assets/acesso-acervo/caixa-fundo.png" alt="Caixa Fundo">
+                <img class="caixa-frente" src="/assets/acesso-acervo/caixa-frente.png" alt="Caixa Frente">
+                <img class="txt-foto" src="/assets/acesso-acervo/txt-foto.png" alt="Texto">
+            </div>
+            <div class="txt-direita">
+                <h2 class="titulo-um">ACESSO AO ACERVO DO ARQUIP</h2>
+                <p class="txt-um">Através de um requerimento eletrônico no Portal de Processos, é possível solicitar a
+                    consulta de
+                    documentos históricos, administrativos ou de interesse público, com a comodidade de recebê-los em
+                    formato digital.
+                </p>
+
+                <h2 class="titulo-dois">VOCÊ SABIA?</h2>
+                <p class="txt-dois">🗂️ O acervo conta com:</p>
+
+                <ul class="lista-um">
+                    <li>Milhares de documentos históricos , como leis, decretos e atas que contam a história da cidade.
+                    </li>
+                    <li>Coleções especiais que guardam relatos e registros exclusivos da vida cotidiana e da
+                        administração pública.</li>
+                    <li>Fotografias e mapas antigos que revelam como a cidade evoluiu ao longo dos anos.</li>
+                </ul>
+            </div>
+            <hr>
+            <div class="retangulo-azul">
+                <img class="retangulo-acervo" src="/assets/acesso-acervo/retangulo-acervo.png" alt="Retangulo Fundo">
+                <img class="quadrado-acervo" src="/assets/acesso-acervo/quadrado-acervo.png" alt="Quadrado">
+                <img class="icone-quadrado" src="/assets/acesso-acervo/icone-quadrado.png" alt="Icone Quadrado">
+
+                <h2 class="titulo-quadrado">COMO SOLICITAR ACESSO AO ACERVO?</h2>
+
+                <img class="seta-esquerda" src="/assets/acesso-acervo/esquerda-acervo.png" alt="Seta Esquerda">
+                <img class="seta-direita" src="/assets/acesso-acervo/direita-acervo.png" alt="Seta Direita">
+
+
+                <p class="txt-quadrado"> <b>1.Acesse o Portal de Processos ARQUIP e faça o login. Utilizamos a SenhaWeb
+                        para acesso ao Portal.<br></b>Caso não possua, siga as orientações em: <a
+                        href="https://capital.sp.gov.br/web/fazenda/servicos/senhaweb">https://capital.sp.gov.br/web/fazenda/servicos/senhaweb</a>
+                </p>
+            </div>
+
+            <div class="txt-esquerda">
+                <img class="img-direita" src="/assets/acesso-acervo/img-direita.png" alt="Imagem Direita">
+                <h2>DÚVIDAS FREQUENTES</h2>
+
+                <h4>Há algum custo para o Serviço?</h4>
+                <p>Sim, para este tipo de serviço há a cobrança da autuação do requerimento eletrônico.</p>
+
+                <h4>Como faço para acompanhar o meu requerimento?</h4>
+                <p class="txt-menor" >Em sua área logada no Portal de Processos, você pode realizar consultas aos processos eletrônicos a
+                    qual você fez autuação, podendo ver seu último andamento e também visualizar os documentos
+                    constituintes destes requerimentos. Além disso, sempre que seu requerimento tiver uma atualização
+                    com informação, você será notificado via endereço eletrônico, o mesmo que está em seu cadastro da
+                    SenhaWeb, fique atento as notificações.
+                </p>
+
+                <h4>Após abrir meu requerimento, quais os próximos passos?</h4>
+                <p>Após a compensação do pagamento, o requerimento é recepcionado em nossa unidade. Faremos a validação
+                    dos dados e, se estiver tudo correto, faremos a solicitação de desarquivamento ao nosso Núcleo
+                    responsável pelos os trabalhos no acervo. Após o desarquivamento, o processo é redirecionado ao
+                    setor de preparação para a digitalização, a qual é ordenado, higienizado e desmetalizado (remoção de
+                    grampos, bailarinas, colchetes, clipes e similares). É então levado ao setor de digitalização,
+                    conferência e autenticação digital. Quando estiver tudo pronto, o setor de atendimento fará a
+                    disponibilização do processo via endereço eletrônico e o seu requerimento será finalizado. Fique
+                    atento aos e-mails e caixa de SPAM.
+                </p>
+
+                <h4>Como poderei ver os processos e documentos solicitados no requerimento?</h4>
+                <p>A vista de processo é feita totalmente no meio digital.</p>
+
+                <h4>Vantagens do Acesso Digital</h4>
+                <ul>
+                    <Li><b>Agilidade e conveniência:</b> Receba os documentos sem a necessidade de deslocamento.</Li>
+                    <Li><b>Preservação e acessibilidade:</b> O acesso digital contribui para a preservação dos
+                        documentos físicos, além de proporcionar acesso rápido e acessível.
+                    </Li>
+                    <Li><b>Segurança e controle de dados:</b> O sistema de consulta respeita normas de sigilo e proteção
+                        de dados pessoais.
+                    </Li>
+                </ul>
+                <p>Após a etapa de digitalização, conforme descrito acima, o processo administrativo físico será
+                    convertido para um processo digital. Sendo assim, ele receberá um novo número de protocolo, sem
+                    prejuízo do número anterior, e os documentos serão inseridos, seguindo a normas e regulamentos de
+                    preservação digital, no Sistema Eletrônicos de Informações. A disponibilização se dará com o envio
+                    de link de acesso para visualizar o processo e seus documentos. Fique atento, pois o link de acesso
+                    é enviado no endereço eletrônico informado no requerimento e tem validade de acesso. Não perca o
+                    prazo!
+                </p>
+
+                <h4>Qual o prazo de atendimento?</h4>
+                <p>De acordo com nossa carta de serviços no SP156, o prazo poderá ser em até 60 dias.</p>
+
+                <p>Veja os nossos tutoriais para orientação.</p>
+
+                <ul>
+                    <li><b>***galeria de pequenos vídeos sobre as orientações dadas acima, vamos listar quais vídeos entrarão,
+                    uns 3 pelo menos: Como solicitar? Como acompanhar? Quais etapas são do atendimento? e Como
+                    visualizar os processos solicitados?</b></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <?php require_once __DIR__ . "/views/footer.php"
+    ?>
+
+
+</body>
+
+</html>
