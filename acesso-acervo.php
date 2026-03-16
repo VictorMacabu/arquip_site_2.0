b445a90ee4e410462d0092ef62af29970f60ea35
João <joaovitoroliveira0711@gmail.com>
2026-03-16 14:18:01 -0300

    Finalizando a página acesso ao acervo

--- acesso-acervo.php
+++ acesso-acervo.php
@@ -7,15 +7,18 @@
     <link rel="stylesheet" href="/CSS/header.css">
     <link rel="stylesheet" href="/CSS/footer.css">
     <link rel="stylesheet" href="/CSS/main-pc.css">
-    <link rel="stylesheet" href="/css/new-acesso.css">
+    <link rel="stylesheet" href="/css/acesso-acervo.css">
     <link rel="shortcut icon" href="/assets/icone.site/Logo.ico" type="image/x-icon">
-    <title>Acesso Ao Acervo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    <title>Acesso ao Acervo</title>
 </head>
 
 <body>
+
     <!-- CABEÇALHO -->
     <?php require_once __DIR__ . "/views/header.php"
     ?>
+
 
     <!-- BANNER -->
     <div class="banner-estrutura">
@@ -36,114 +39,167 @@
         </div>
     </div>
 
-    <!-- INICIO PÁGINA -->
-    <div class="container-pagina">
-        <div class="card-secao">
-            <div class="img-acervo">
-                <img src="/assets/acesso-acervo/img-um.png" alt="">
-            </div>
-            
-            <div class="txt-direita">
-                <h2 class="titulo-um">ACESSO AO ACERVO DO ARQUIP</h2>
-                <p class="txt-um">Através de um requerimento eletrônico no Portal de Processos, é possível solicitar a
-                    consulta de
-                    documentos históricos, administrativos ou de interesse público, com a comodidade de recebê-los em
-                    formato digital.
-                </p>
-
-                <h2 class="titulo-dois">VOCÊ SABIA?</h2>
-                <p class="txt-dois">🗂️ O acervo conta com:</p>
-
-                <ul class="lista-um">
-                    <li>Milhares de documentos históricos , como leis, decretos e atas que contam a história da cidade.
-                    </li>
-                    <li>Coleções especiais que guardam relatos e registros exclusivos da vida cotidiana e da
-                        administração pública.</li>
-                    <li>Fotografias e mapas antigos que revelam como a cidade evoluiu ao longo dos anos.</li>
-                </ul>
-            </div>
-            <div class="retangulo-azul">
-                <img class="retangulo-acervo" src="/assets/acesso-acervo/retangulo-acervo.png" alt="Retangulo Fundo">
-                <img class="quadrado-acervo" src="/assets/acesso-acervo/quadrado-acervo.png" alt="Quadrado">
-                <img class="icone-quadrado" src="/assets/acesso-acervo/icone-quadrado.png" alt="Icone Quadrado">
-
-                <h2 class="titulo-quadrado">COMO SOLICITAR ACESSO AO ACERVO?</h2>
-
-                <img class="seta-esquerda" src="/assets/acesso-acervo/esquerda-acervo.png" alt="Seta Esquerda">
-                <img class="seta-direita" src="/assets/acesso-acervo/direita-acervo.png" alt="Seta Direita">
 
 
-                <p class="txt-quadrado"> <b>1.Acesse o Portal de Processos ARQUIP e faça o login. Utilizamos a SenhaWeb
-                        para acesso ao Portal.<br></b>Caso não possua, siga as orientações em: <a
-                        href="https://capital.sp.gov.br/web/fazenda/servicos/senhaweb">https://capital.sp.gov.br/web/fazenda/servicos/senhaweb</a>
-                </p>
-            </div>
+    <!-- ACESSO AO ACERVO / INICIO -->
+    <main>
+        <section class="acesso-acervo">
+            <div>
 
-            <div class="txt-esquerda">
-                <img class="img-direita" src="/assets/acesso-acervo/img-direita.png" alt="Imagem Direita">
-                <h2>DÚVIDAS FREQUENTES</h2>
+                <div class="acesso-acervo-arquip">
+                    <div class="img-acervo">
+                        <img src="/assets/acesso-acervo/img-um.png" alt="Imagem do Acervo do Arquip">
+                    </div>
+                    <div class="acesso-acervo_conteudo_principal">
+                        <h2 class="titulo-acervo_arquip">ACESSO AO ACERVO DO ARQUIP</h2>
+                        <p class="conteudo-acervo_arquip">
+                            Através de um requerimento eletrônico no Portal de Processos, é possível solicitar a
+                            consulta de
+                            documentos históricos, administrativos ou de interesse público, com a comodidade de recebê-los em
+                            formato digital.
+                        </p>
 
-                <h4>Há algum custo para o Serviço?</h4>
-                <p>Sim, para este tipo de serviço há a cobrança da autuação do requerimento eletrônico.</p>
+                        <h2 class="titulo-voce_sabia">VOCÊ SABIA?</h2>
+                        <p class="texto-acervo_conta">🗂️ O acervo conta com:</p>
 
-                <h4>Como faço para acompanhar o meu requerimento?</h4>
-                <p class="txt-menor" >Em sua área logada no Portal de Processos, você pode realizar consultas aos processos eletrônicos a
-                    qual você fez autuação, podendo ver seu último andamento e também visualizar os documentos
-                    constituintes destes requerimentos. Além disso, sempre que seu requerimento tiver uma atualização
-                    com informação, você será notificado via endereço eletrônico, o mesmo que está em seu cadastro da
-                    SenhaWeb, fique atento as notificações.
-                </p>
+                        <ul class="lista-acervo_conta">
+                            <li>
+                                Milhares de documentos históricos , como leis, decretos e atas que contam a história da cidade.
+                            </li>
+                            <li>
+                                Coleções especiais que guardam relatos e registros exclusivos da vida cotidiana e da
+                                administração pública.
+                            </li>
+                            <li>
+                                Fotografias e mapas antigos que revelam como a cidade evoluiu ao longo dos anos.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
 
-                <h4>Após abrir meu requerimento, quais os próximos passos?</h4>
-                <p>Após a compensação do pagamento, o requerimento é recepcionado em nossa unidade. Faremos a validação
-                    dos dados e, se estiver tudo correto, faremos a solicitação de desarquivamento ao nosso Núcleo
-                    responsável pelos os trabalhos no acervo. Após o desarquivamento, o processo é redirecionado ao
-                    setor de preparação para a digitalização, a qual é ordenado, higienizado e desmetalizado (remoção de
-                    grampos, bailarinas, colchetes, clipes e similares). É então levado ao setor de digitalização,
-                    conferência e autenticação digital. Quando estiver tudo pronto, o setor de atendimento fará a
-                    disponibilização do processo via endereço eletrônico e o seu requerimento será finalizado. Fique
-                    atento aos e-mails e caixa de SPAM.
-                </p>
 
-                <h4>Como poderei ver os processos e documentos solicitados no requerimento?</h4>
-                <p>A vista de processo é feita totalmente no meio digital.</p>
+                <div class="banner-azul">
+                    <div class="banner-azul_solicitar">
+                        <img src="/assets/acesso-acervo/icone-documento.png" alt="Icone de documento">
+                        <h2 class="titulo-quadrado">
+                            COMO SOLICITAR ACESSO AO ACERVO?
+                        </h2>
+                    </div>
 
-                <h4>Vantagens do Acesso Digital</h4>
-                <ul>
-                    <Li><b>Agilidade e conveniência:</b> Receba os documentos sem a necessidade de deslocamento.</Li>
-                    <Li><b>Preservação e acessibilidade:</b> O acesso digital contribui para a preservação dos
-                        documentos físicos, além de proporcionar acesso rápido e acessível.
-                    </Li>
-                    <Li><b>Segurança e controle de dados:</b> O sistema de consulta respeita normas de sigilo e proteção
-                        de dados pessoais.
-                    </Li>
-                </ul>
-                <p>Após a etapa de digitalização, conforme descrito acima, o processo administrativo físico será
-                    convertido para um processo digital. Sendo assim, ele receberá um novo número de protocolo, sem
-                    prejuízo do número anterior, e os documentos serão inseridos, seguindo a normas e regulamentos de
-                    preservação digital, no Sistema Eletrônicos de Informações. A disponibilização se dará com o envio
-                    de link de acesso para visualizar o processo e seus documentos. Fique atento, pois o link de acesso
-                    é enviado no endereço eletrônico informado no requerimento e tem validade de acesso. Não perca o
-                    prazo!
-                </p>
+                    <div class="login">
+                        <div>
+                            <button>
+                                <img class="seta-esquerda" src="/assets/acesso-acervo/esquerda-acervo.png" alt="Seta Esquerda">
+                            </button>
+                        </div>
+                        <div class="texto-login">
+                            <p class="acessar">
+                                1.Acesse o Portal de Processos ARQUIP e faça o login. Utilizamos a SenhaWeb
+                                para acesso ao Portal.<br>
+                            </p>
+                            <p class="orientacoes">
+                                Caso não possua, siga as orientações em:
+                                <a
+                                    href="https://capital.sp.gov.br/web/fazenda/servicos/senhaweb" target="blanck">https://capital.sp.gov.br/web/fazenda/servicos/senhaweb
+                                </a>
+                            </p>
+                        </div>
+                        <div>
+                            <button>
+                                <img class="seta-direita" src="/assets/acesso-acervo/direita-acervo.png" alt="Seta Direita">
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
-                <h4>Qual o prazo de atendimento?</h4>
-                <p>De acordo com nossa carta de serviços no SP156, o prazo poderá ser em até 60 dias.</p>
 
-                <p>Veja os nossos tutoriais para orientação.</p>
+                <!--- DÚVIDAS FREQUENTES --->
+                <div class="duvidas-frequentes">
+                    <div class="primeira-secao">
+                        <div class="img-acervo">
+                            <img class="pessoal-trabalhando" src="/assets/acesso-acervo/img-direita.png" alt="Imagem Direita">
+                        </div>
+                        <div class="duvidas">
+                            <h2>DÚVIDAS FREQUENTES</h2>
+                            <div class="duvidas-custos">
+                                <h4>Há algum custo para o Serviço?</h4>
+                                <p>Sim, para este tipo de serviço há a cobrança da autuação do requerimento eletrônico.</p>
+                            </div>
 
-                <ul>
-                    <li><b>***galeria de pequenos vídeos sobre as orientações dadas acima, vamos listar quais vídeos entrarão,
-                    uns 3 pelo menos: Como solicitar? Como acompanhar? Quais etapas são do atendimento? e Como
-                    visualizar os processos solicitados?</b></li>
-                </ul>
-            </div>
-        </div>
-    </div>
+                            <div class="duvidas-requerimento">
+                                <h4>Como faço para acompanhar o meu requerimento?</h4>
+                                <p>Em sua área logada no Portal de Processos, você pode realizar consultas aos processos eletrônicos a
+                                    qual você fez autuação, podendo ver seu último andamento e também visualizar os documentos
+                                    constituintes destes requerimentos. Além disso, sempre que seu requerimento tiver uma atualização
+                                    com informação, você será notificado via endereço eletrônico, o mesmo que está em seu cadastro da
+                                    SenhaWeb, fique atento as notificações.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="duvidas-primeiros_passos">
+                        <h4>
+                            Após abrir meu requerimento, quais os próximos passos?
+                        </h4>
+                        <p>
+                            Após a compensação do pagamento, o requerimento é recepcionado em nossa unidade. Faremos a validação
+                            dos dados e, se estiver tudo correto, faremos a solicitação de desarquivamento ao nosso Núcleo
+                            responsável pelos os trabalhos no acervo. Após o desarquivamento, o processo é redirecionado ao
+                            setor de preparação para a digitalização, a qual é ordenado, higienizado e desmetalizado (remoção de
+                            grampos, bailarinas, colchetes, clipes e similares). É então levado ao setor de digitalização,
+                            conferência e autenticação digital. Quando estiver tudo pronto, o setor de atendimento fará a
+                            disponibilização do processo via endereço eletrônico e o seu requerimento será finalizado. Fique
+                            atento aos e-mails e caixa de SPAM.
+                        </p>
+                    </div>
+
+                    <div class="duvidas-documentos_solicitados">
+                        <h4>Como poderei ver os processos e documentos solicitados no requerimento?</h4>
+                        <p>A vista de processo é feita totalmente no meio digital.</p>
+                    </div>
+
+                    <div class="duvidas-vantagens">
+                        <h4>Vantagens do Acesso Digital</h4>
+                        <ul>
+                            <Li><b>Agilidade e conveniência:</b> Receba os documentos sem a necessidade de deslocamento.</Li>
+                            <Li><b>Preservação e acessibilidade:</b> O acesso digital contribui para a preservação dos
+                                documentos físicos, além de proporcionar acesso rápido e acessível.
+                            </Li>
+                            <Li><b>Segurança e controle de dados:</b> O sistema de consulta respeita normas de sigilo e proteção
+                                de dados pessoais.
+                            </Li>
+                        </ul>
+                        <p>Após a etapa de digitalização, conforme descrito acima, o processo administrativo físico será
+                            convertido para um processo digital. Sendo assim, ele receberá um novo número de protocolo, sem
+                            prejuízo do número anterior, e os documentos serão inseridos, seguindo a normas e regulamentos de
+                            preservação digital, no Sistema Eletrônicos de Informações. A disponibilização se dará com o envio
+                            de link de acesso para visualizar o processo e seus documentos. Fique atento, pois o link de acesso
+                            é enviado no endereço eletrônico informado no requerimento e tem validade de acesso. Não perca o
+                            prazo!
+                        </p>
+                    </div>
+
+                    <div class="duvidas-prazo">
+                        <h4>Qual o prazo de atendimento?</h4>
+                        <p>De acordo com nossa carta de serviços no SP156, o prazo poderá ser em até 60 dias.</p>
+
+                        <p>Veja os nossos tutoriais para orientação.</p>
+
+                        <ul>
+                            <li><b>***galeria de pequenos vídeos sobre as orientações dadas acima, vamos listar quais vídeos entrarão,
+                                    uns 3 pelo menos: Como solicitar? Como acompanhar? Quais etapas são do atendimento? e Como
+                                    visualizar os processos solicitados?</b></li>
+                        </ul>
+                    </div>
+                </div>
+        </section>
+    </main>
 
     <!-- RODAPÉ -->
     <?php require_once __DIR__ . "/views/footer.php"
     ?>
-    
+
 </body>
-</html>   
+
+</html>
