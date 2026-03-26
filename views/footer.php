7b0422f129bc0dda178f7d45639181b3909c0fbd
GuGiavera <gustavobuenosk@gmail.com>
2026-03-26 09:01:40 -0300

    style: Atualização estetica do footer

--- views/footer.php
+++ views/footer.php
@@ -3,18 +3,16 @@
         <p><a href="https://prefeitura.sp.gov.br/web/gestao" target="_blank" style="color: #fff">Secretaria Municipal de Gestão</a></p>
         <p><b>ARQUIVO PÚBLICO MUNICIPAL</b></p> 
         <p class="nome-arquip">Jornalista Paulo Roberto Dutra</p> 
-        <p> <a href="https://maps.app.goo.gl/zMx25UhoKkd3BmAR7" target="_blank" style="color: #fff">R. Léo Ribeiro de Morães, 66 - Freguesia do Ó <br> São Paulo/SP- Brasil</a></p>
-    </address>
-
-    
-     <address class="prefeitura">
-        <img src="/assets/footer/logo-prefeitura.png" alt="Logo Prefeitura">
+        <p> <a href="https://maps.app.goo.gl/zMx25UhoKkd3BmAR7" target="_blank" style="color: #fff">R. Léo Ribeiro de Morães, 66 - Freguesia do Ó - São Paulo/SP- Brasil</a></p>
     </address>
 
     <address class="arquip">
         <img src="/assets/footer/logo-arquip.png" alt="Logo Arquip">
     </address>
 
+    <address class="prefeitura">
+        <img src="/assets/footer/logo-prefeitura.png" alt="Logo Prefeitura">
+    </address>
 
     <address class="contatos">
         <div class="footer-direita">
@@ -22,7 +20,7 @@
             <p>Desenvolvido por Arquip ©</p>
             <p>Atualizado em 08/01/2025</p>
             <p>Contato:<a href="arquip@prefeitura.sp.gov.br" target="_blank" style="color: #fff;"> arquip@prefeitura.sp.gov.br</a></p>
-            <p><a href="atendimento.php" target="_blank" style="color: #fff">Atendimento Virtual</a></p>
+            <p><a href="atendimento.php" target="_blank" ">Atendimento Virtual</a></p>
         </div>
     </address>
 </footer>
