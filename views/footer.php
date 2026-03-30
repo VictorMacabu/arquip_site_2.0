9a55257dd8c447126787539d9ca86a390afd7d82
GuGiavera <gustavobuenosk@gmail.com>
2026-03-30 10:24:19 -0300

    style: atualização de cores dos textos e alinhamento das imagens

--- views/footer.php
+++ views/footer.php
@@ -1,9 +1,9 @@
 <footer>
     <address class="info-footer">
-        <p><a href="https://prefeitura.sp.gov.br/web/gestao" target="_blank" style="color: #fff">Secretaria Municipal de Gestão</a></p>
+        <p><a href="https://prefeitura.sp.gov.br/web/gestao" target="_blank" style="color: #64748B">Secretaria Municipal de Gestão</a></p>
         <p><b>ARQUIVO PÚBLICO MUNICIPAL</b></p> 
-        <p class="nome-arquip">Jornalista Paulo Roberto Dutra</p> 
-        <p> <a href="https://maps.app.goo.gl/zMx25UhoKkd3BmAR7" target="_blank" style="color: #fff">R. Léo Ribeiro de Morães, 66 - Freguesia do Ó - São Paulo/SP- Brasil</a></p>
+        <p style="color: #64748B" class="nome-arquip">Jornalista Paulo Roberto Dutra</p> 
+        <p> <a href="https://maps.app.goo.gl/zMx25UhoKkd3BmAR7" target="_blank" style="color: #CBD5E1">R. Léo Ribeiro de Morães, 66 - Freguesia do Ó - São Paulo/SP- Brasil</a></p>
     </address>
 
     <address class="arquip">
@@ -16,11 +16,11 @@
 
     <address class="contatos">
         <div class="footer-direita">
-            <p><a href="mapa-site.php" target="_blank" style="color: #fff;">Mapa do Site</a></p>
-            <p>Desenvolvido por Arquip ©</p>
-            <p>Atualizado em 08/01/2025</p>
-            <p>Contato:<a href="arquip@prefeitura.sp.gov.br" target="_blank" style="color: #fff;"> arquip@prefeitura.sp.gov.br</a></p>
-            <p><a href="atendimento.php" target="_blank" ">Atendimento Virtual</a></p>
+            <p><a href="mapa-site.php" target="_blank" style="color: #CBD5E1;">Mapa do Site</a></p>
+            <p style="color: #64748B">Desenvolvido por Arquip ©</p>
+            <p style="color: #64748B">Atualizado em 08/01/2025</p>
+            <p style="color: #CBD5E1>Contato:<a href="arquip@prefeitura.sp.gov.br" target="_blank" style="color: #fff;"> arquip@prefeitura.sp.gov.br</a></p>
+            <p><a href="atendimento.php" target="_blank" style="color: #0056B3">Atendimento Virtual</a></p>
         </div>
     </address>
 </footer>
