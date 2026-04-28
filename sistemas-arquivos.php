35ae89ed822f1478b054b96e7567ce73a21adf4b
GuGiavera <gustavobuenosk@gmail.com>
2026-04-28 12:42:17 -0300

    ref: atualização da página Sistemas de Arquivos do Municipio

--- sistemas-arquivos.php
+++ sistemas-arquivos.php
@@ -14,89 +14,190 @@
 </head>
 
 <body>
-    <div class="banner-estrutura">
-        <div class="icone-esquerda">
-            <img class="img-esquerda" src="/assets/banner/sistema-de-arquivos/jovem-trabalhando.png"
-                alt="Jovem trabalhando">
-        </div>
-
-        <div class="texto-banner">
-            <h1>
-                SISTEMA DE ARQUIVOS DO MUNICÍPIO
-            </h1>
-            <p>
-                O Decreto nº 57.783, de 13 de julho de 2017, regulamenta a gestão e preservação de <br> documentos na
-                administração pública municipal de São Paulo, garantindo o tratamento <br> adequado dos documentos
-                públicos ao longo de seu ciclo de vida.
-            </p>
-        </div>
-
-        <div>
-            <img class="img-direita" src="/assets/banner/sistema-de-arquivos/icone-documentos.png"
-                alt="Icone de documento">
-        </div>
-    </div>
-
     <!-- SISTEMA DE ARQUIVOS -->
     <main>
         <div class="estrutura-textos">
+            <img class="img-sistema" src="/assets/sistemas-arquivos/foto-sistema.png" alt="Foto Sistema">
             <section class="sistema-arquivos">
-                <h2 class="sistema-arquivo_titulo">
-                    Sistema de Arquivos do Município de São Paulo - SAMSP
-                </h2>
-                <div class="sistema-arquivos_conteudo">
-                    <p class="sistema-arquivo_texto">
-                        O decreto define a criação do SAMSP, uma rede que integra todos os arquivos e documentos da
-                        administração
-                        pública municipal. Esse sistema visa garantir a coordenação e padronização das ações de gestão
-                        documental, além de assegurar o acesso público a documentos que não sejam sigilosos.
-                    </p>
+                <div class="sistema-txt">
+                    <h1>Sistemas de Arquivos do Município</h1>
 
-                    <p class="sistema-arquivo_texto">
-                        O Sistema de Arquivos do Município de São Paulo deve incluir um conjunto de normas técnicas e
-                        operacionais para a organização, preservação e gestão de documentos nos órgãos municipais. Ele
-                        está
-                        intimamente ligado ao ARQUIP (Arquivo Público Municipal), que é responsável pela coordenação e
-                        implementação dessas políticas como Órgão Central.
-                    </p>
-
-                    <p class="sistema-arquivo_texto">
-                        O decreto estabelece a política de gestão de documentos na administração pública municipal, com
-                        o intuito
-                        de garantir que os documentos sejam produzidos, organizados, acessíveis, preservados e, quando
-                        necessário, eliminados de maneira adequada. Ele abrange tanto os documentos físicos quanto os
-                        digitais,
-                        promovendo a integração entre os diversos órgãos da administração municipal para a gestão
-                        eficiente dos
-                        arquivos.
+                    <p>O Decreto nº 57.783, de 13 de julho de 2017, regulamenta a gestão e preservação de documentos na
+                        administração pública municipal de São Paulo, garantindo o tratamento adequado dos documentos
+                        públicos ao longo de seu ciclo de vida.
                     </p>
                 </div>
             </section>
-
             <section class="definicao-documental">
-                <h2 class="definicao-documental_subtitulo">
-                    Definição de Gestão Documental
-                </h2>
+                <div class="txt-definicao">
+                    <h2>Definição de Gestão Documental</h2>
 
-                <p>
-                    A gestão de documentos, conforme o decreto, envolve o tratamento de documentos públicos em todas as
-                    fases
-                    do seu ciclo de vida, que inclui:
-                </p>
+                    <p>O decreto define a criação do SAMSP, uma rede que integra todos os arquivos e documentos da
+                        administração pública municipal. Esse sistema visa garantir a coordenação e padronização das
+                        ações de gestão documental, além de assegurar o acesso público a documentos que não sejam
+                        sigilosos.
+                    </p>
 
-                <ul class="definicao-documental_lista">
-                    <li><b>Produção:</b> Criação ou recebimento dos documentos.</li>
-                    <li><b>Trâmite:</b> Circulação ou movimentação dentro da administração.</li>
-                    <li><b>Arquivamento:</b> Armazenamento e organização dos documentos.</li>
-                    <li><b>Preservação:</b> Manutenção dos documentos por meio de ações que garantam sua integridade ao
-                        longo do tempo.</li>
-                    <li><b>Eliminação ou transferência:</b> Destinação final, que pode ser a eliminação ou a
-                        transferência para arquivos permanentes.</li>
-                </ul>
+                    <p class="txt-img">O Sistema de Arquivos do Município de São Paulo deve incluir um conjunto de
+                        normas técnicas e
+                        operacionais para a organização, preservação e gestão de documentos nos órgãos municipais. Ele
+                        está intimamente ligado ao ARQUIP (Arquivo Público Municipal), que é responsável pela
+                        coordenação e implementação dessas políticas como Órgão Central.
+                    </p>
+
+                    <p>O decreto estabelece a política de gestão de documentos na administração pública municipal, com o
+                        intuito de garantir que os documentos sejam produzidos, organizados, acessíveis, preservados e,
+                        quando necessário, eliminados de maneira adequada. Ele abrange tanto os documentos físicos
+                        quanto os digitais, promovendo a integração entre os diversos órgãos da administração municipal
+                        para a gestão eficiente dos arquivos.
+                    </p>
+                </div>
+            </section>
+            <section class="sistema-samsp">
+                <div class="txt-samsp">
+                    <h3>Sistema de Arquivos do Município de São Paulo - SAMSP</h3>
+
+                    <p>A gestão de documentos, conforme o decreto, envolve o tratamento de documentos públicos em todas
+                        as fases do seu ciclo de vida, que inclui:
+                    </p>
+                </div>
+
+                <!-- CARDS -->
+                <div class="imagens-samsp">
+                    <div class="icone-um">
+                        <img src="/assets/sistemas-arquivos/icone-um.png" alt="Icone">
+                    </div>
+
+                    <div class="cards-sistemas">
+                        <div class="card-sistema">
+                            <div class="card-header">
+                                <img src="/assets/sistemas-arquivos/azul-card.png" alt="Azul do Card">
+                                <div class="numero">
+                                    <p>2</p>
+                                </div>
+                            </div>
+                            <div class="conteudo-card">
+                                <div class="titulo">
+                                    <p>Trâmite</p>
+                                </div>
+
+                                <div class="paragrafo">
+                                    <p>Circulação ou movimentação dentro da administração.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="icone-tres">
+                        <img src="/assets/sistemas-arquivos/icone-tres.png" alt="Icone">
+                    </div>
+
+                    <div class="cards-sistemas">
+                        <div class="card-sistema">
+                            <div class="card-header">
+                                <img src="/assets/sistemas-arquivos/azul-card.png" alt="Azul do Card">
+                                <div class="numero">
+                                    <p>4</p>
+                                </div>
+                            </div>
+                            <div class="conteudo-card">
+                                <div class="titulo">
+                                    <p>Preservação</p>
+                                </div>
+
+                                <div class="paragrafo">
+                                    <p>Manutenção dos documentos por meio de ações que garantam sua integridade ao longo
+                                        do tempo.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="icone-cinco">
+                        <img src="/assets/sistemas-arquivos/icone-cinco.png" alt="Icone">
+                    </div>
+                </div>
+
+                <div class="divisao-samsp">
+                    <img src="/assets/sistemas-arquivos/divisao-card.png" alt="Divisão">
+                </div>
+
+                <div class="imagens-samsp">
+
+                    <div class="cards-sistemas">
+                        <div class="card-sistema">
+                            <div class="card-header">
+                                <img src="/assets/sistemas-arquivos/azul-card.png" alt="Azul do Card">
+                                <div class="numero">
+                                    <p>1</p>
+                                </div>
+                            </div>
+                            <div class="conteudo-card">
+                                <div class="titulo">
+                                    <p>Produção</p>
+                                </div>
+
+                                <div class="paragrafo">
+                                    <p>Criação ou recebimento dos documentos.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="icone-dois">
+                        <img src="/assets/sistemas-arquivos/icone-dois.png" alt="Icone">
+                    </div>
+
+                    <div class="cards-sistemas">
+                        <div class="card-sistema">
+                            <div class="card-header">
+                                <img src="/assets/sistemas-arquivos/azul-card.png" alt="Azul do Card">
+                                <div class="numero">
+                                    <p>3</p>
+                                </div>
+                            </div>
+                            <div class="conteudo-card">
+                                <div class="titulo">
+                                    <p>Arquivamento</p>
+                                </div>
+
+                                <div class="paragrafo">
+                                    <p>Armazenamento e organização dos documentos.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="icone-quatro">
+                        <img src="/assets/sistemas-arquivos/icone-quatro.png" alt="Icone">
+                    </div>
+
+                    <div class="cards-sistemas">
+                        <div class="card-sistema">
+                            <div class="card-header">
+                                <img src="/assets/sistemas-arquivos/azul-card.png" alt="Azul do Card">
+                                <div class="numero">
+                                    <p>5</p>
+                                </div>
+                            </div>
+                            <div class="conteudo-card">
+                                <div class="titulo">
+                                    <p>Eliminação/Transfêrencia</p>
+                                </div>
+
+                                <div class="paragrafo">
+                                    <p>Destinação final, que pode ser a eliminação ou a transferência para arquivos
+                                        permanentes.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </section>
         </div>
     </main>
-    
+
     <script src="js/script.js"></script>
 
 </body>
