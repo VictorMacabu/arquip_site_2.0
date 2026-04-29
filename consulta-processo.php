88a79d37a131ade0b7ab870e668e8054c50321cf
João <joaovitoroliveira0711@gmail.com>
2026-04-29 16:04:07 -0300

    Finalização da Página Consulta de Processos.

--- consulta-processo.php
+++ consulta-processo.php
@@ -4,70 +4,146 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/consulta-processo.css">
     <link rel="stylesheet" href="/css/main-pc.css">
+    <link rel="stylesheet" href="/css/consulta-processo.css">
     <title>Consulta de Processos</title>
 </head>
 
 <body>
-    <!--- Banner --->
-    <div class="banner-estrutura">
-        <div>
-            <img class="img-esquerda" src="/assets/consulta-processos/pasta-trabalho.png" alt="Imagem da legislação">
-        </div>
-        <div class="texto-banner">
-            <h1>CONSULTA DE PROCESSOS ADMINISTRATIVOS</h1>
-            <p>
-                A consulta de processos por número no portal de processos serve para facilitar o acesso e o acompanhamento de informações sobre processos administrativos e judiciais.
-            </p>
-        </div>
-        <div>
-            <img class="img-direita" src="/assets/consulta-processos/icone-pesquisa.png" alt="Icone da legislação">
-        </div>
-    </div>
-
-    <!--- Consulta de Processos Administrativos --->
     <main>
-        <section class="portal-de_processos">
-            <div class="card-portal">
-                <div class="card-portal_conteudo">
-                    <h1>PORTAL DE PROCESSOS</h1>
-                    <P>
-                        Essa ferramenta de consulta foi desenvolvida para facilitar o acesso rápido e eficiente a informações sobre processos específicos.
-                    </P>
+        <section class="container-consulta_processos">
 
-                    <P>
-                        Com ela, cidadãos, servidores públicos e outras partes interessadas podem localizar detalhes e acompanhar o status de um processo de forma prática, utilizando apenas o número identificado ou único.
-                    </P>
-                </div>
-            </div>
-
-            <div class="consulta-processos">
-                <div class="consulta-processos_conteudo">
-                    <div class="consulta-processos_titulosub">
-                        <h2>COMO CONSULTAR O SEU PROCESSO ADMINISTRATIVOS?</h2>
+            <div class="consulta-processo">
+                <div class="consulta-processo_conteudo">
+                    <div class="consulta-processo_titulo">
+                        <h1>Consulta de Processos Administrativos</h1>
+                    </div>
+                    <div class="consulta-processo_texto">
                         <p>
-                            Para realizar a consulta por número, basta acessar o portal de processos em:
+                            A consulta de processos por número no portal de processos serve para facilitar o acesso e o acompanhamento de informações sobre processos administrativos e judiciais.
                         </p>
                     </div>
-                    <div class="consulta-processos_link">
-                        <img src="/assets/consulta-processos/internet.png" alt="Iconde de Rede (Busca em Internet)">
-                        <a href="https://www.prefeitura.sp.gov.br/" target="_blank">www.processos.prefeitura.sp.gov.br/</a>
-                        <img src="/assets/consulta-processos/lupa.png" alt="Icone de Lupa para pesquisa">
+                    <div class="consulta-processo_botao">
+                        <button class="btn">
+                            Consultar Processo Agora
+                        </button>
+                        <button class="btn">
+                            Ver Como Funciona
+                        </button>
                     </div>
-                    <h3>QUER SABER PASSO A PASSO? ASSISTA OS TUTORIAIS ABAIXO:</h3>
                 </div>
-                <div class="consulta-processos_img">
-                    <img src="/assets/consulta-processos/img-homem.png" alt="Imagem de um Homem olhando o celular">
+                <div class="consulta-processo_img">
+                    <img src="/assets/consulta-processos/notebook.png" alt="Imagem de um notebook">
                 </div>
             </div>
 
-            <div class="portal-de_processos_video">
-                <img src="/assets/consulta-processos/img-cinza.png" alt="Video de como consultar o Processo Administrativos">
+            <div class="selecao-metodo">
+                <div class="selecao-metodo_titulo">
+                    <h2>Como Deseja Consultar?</h2>
+                    <p>Selecione o método de identificação mais conveniente para você</p>
+                </div>
+
+                <div class="selecao-metodo_cards">
+                    <div class="card">
+                        <img src="/assets/consulta-processos/jogo_velha.png" alt="Img do jogo da velha">
+                        <h3>Por número do Processo</h3>
+                        <p>
+                            Utilize o número do protocolo gerado no momento da
+                            abertura da solicitação.
+                        </p>
+                        <div class="btn-card">
+                            <a href="https://processos.prefeitura.sp.gov.br/Forms/consultarProcessos.aspx" target="_blank">Acessar</a>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <img src="/assets/consulta-processos/cartao.png" alt="">
+                        <h3>Por CPF/CNPJ</h3>
+                        <p>
+                            Consulte todos os processos vinculados ao seu
+                            documento de identificação.
+                        </p>
+                        <div class="btn-card">
+                            <a href="https://processos.prefeitura.sp.gov.br/Forms/consultarProcessos.aspx?id=I" target="_blank">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="portal-processo">
+                <div class="portal-processo_conteudo">
+                    <div class="portal-processo_titulo">
+                        <h2>Portal de Processos</h2>
+                    </div>
+                    <div class="portal-processo_texto">
+                        <p>
+                            Essa ferramenta de consulta foi desenvolvida para facilitar o acesso rápido e eficiente a informações sobre processos específicos.
+                            Com ela, cidadãos, servidores públicos e outras partes interessadas podem localizar detalhes e acompanhar o status de um processo de forma prática, utilizando apenas o número identificado ou único.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="portal-processo_img">
+                    <img src="/assets/consulta-processos/trabalho.png" alt="Imagem ilustrativa do pessoal trabalhando">
+                </div>
+            </div>
+
+
+            <div class="tutoriais">
+                <div class="tutoriais-header">
+                    <div class="consulta-processo_titulo_sub">
+                        <h2>Aprenda a Consultar</h2>
+                        <p>Guias rápidos em vídeo para facilitar seu acesso</p>
+                    </div>
+
+                    <div class="tutoriais-link">
+                        <a href="#">
+                            <h3>Ver todos os tutoriais</h3>
+                            <span class="seta"></span>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="tutoriais-cards">
+                    <div class="busca-numero">
+                        <div class="consulta-processo_img">
+                            <img src="/assets/consulta-processos/video1.png" alt="">
+                        </div>
+                        <h3>Como consultar por número de processo</h3>
+                    </div>
+                    <div class="busca-cpf-cnpj">
+                        <div class="consulta-processo_img">
+                            <img src="/assets/consulta-processos/video1.png" alt="">
+                        </div>
+                        <h3>Busca por CPF ou CNPJ</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="banner-portal">
+                <div class="shape shape-top"></div>
+                <div class="shape shape-bottom"></div>
+
+                <div class="banner-portal_conteudo">
+                    <div class="banner-portal_titulo">
+                        <h2>Acesse agora o portal e consulte seu processo</h2>
+                    </div>
+                    <div class="banner-portal_texto">
+                        <p>
+                            Ganhe tempo e evite deslocamentos desnecessários. Toda a prefeitura agora
+                            está a um clique de distância de você.
+                        </p>
+                    </div>
+                    <div class="banner-portal_botao">
+                        <a href="https://processos.prefeitura.sp.gov.br/Forms/Principal.aspx" target="_blank" class="banner-portal_btn">
+                            Ir para Área de Consulta
+                        </a>
+                    </div>
+                </div>
             </div>
         </section>
     </main>
-
 </body>
 
 </html>
